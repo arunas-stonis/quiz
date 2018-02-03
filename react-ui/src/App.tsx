ae6b9d26a5dd3890b92154836139c6bf1a3234cd
arunas <arunas.stonis@gmail.com>
2018-02-03 14:27:39 +0200

    react-create-app on Heroku

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -3,16 +3,58 @@ import './App.css';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component {
+interface AppState {
+  message: string | null;
+  fetching: boolean;
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      message: null,
+      fetching: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        this.setState({
+          message: json.message,
+          fetching: false
+        });
+      }).catch(e => {
+        this.setState({
+          message: `API call failed: ${e}`,
+          fetching: false
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h2>Welcome to React</h2>
+        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          {'This is '}
+          <a href="https://github.com/mars/heroku-cra-node">
+            {'create-react-app with a custom Node/Express server'}
+          </a><br/>
+        </p>
+        <p className="App-intro">
+          {this.state.fetching
+            ? 'Fetching message from API'
+            : this.state.message}
         </p>
       </div>
     );
